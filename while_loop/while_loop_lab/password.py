c3634f34564e8e3854cb3b9e3105932afc8f7bc0
Vasil Dimitrov <142613528+Zeus097@users.noreply.github.com>
2024-01-17 16:30:15 +0200

    Delete while_loop directory

--- while_loop/while_loop_lab/password.py
+++
@@ -1,8 +0,0 @@
-name = input()
-password = input()
-data = input()
-
-while password != data:
-    data = input()
-
-print(f"Welcome {name}!")
--- while_loop/while_loop_lab/sum_numbers.py
+++
@@ -1,9 +0,0 @@
-number = int(input())
-sum = 0
-
-while True:
-    extra_number = int(input())
-    sum += extra_number
-    if sum >= number:
-        print(sum)
-        break
