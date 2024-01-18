8586f62f5dd700aaaf40911a3f07fe127a854dd2
Vasil Dimitrov <142613528+Zeus097@users.noreply.github.com>
2024-01-18 22:47:06 +0200

    Delete Python Fundamentals/Data_Types_and_Variables/Lab directory

--- Python Fundamentals/Data_Types_and_Variables/Lab/concat_names.py
+++
@@ -1,5 +0,0 @@
-name = input()
-surname = input()
-delimiter = input()
-
-print(f"{name}{delimiter}{surname}")
--- Python Fundamentals/Data_Types_and_Variables/Lab/next_happy_year_solution_1a.py
+++
@@ -1,9 +0,0 @@
-year = int(input())
-
-while True:
-    year += 1
-    year_as_string = str(year)
-    if len(set(year_as_string)) == len(year_as_string):
-        break
-
-print(year)
