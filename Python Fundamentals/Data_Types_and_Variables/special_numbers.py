9661b8c1cc6a10bedfad7308ddbb416950433759
Vasil Dimitrov <142613528+Zeus097@users.noreply.github.com>
2024-01-18 23:13:50 +0200

    Add files via upload

---
+++ Python Fundamentals/Data_Types_and_Variables/special_numbers.py
@@ -0,0 +1,10 @@
+number = int(input())
+
+for current_number in range(1, number + 1):
+    is_special = False
+    sum_digits_sum = 0
+    for digit in str(current_number):
+        sum_digits_sum += int(digit)
+    if sum_digits_sum == 5 or sum_digits_sum == 7 or sum_digits_sum == 11:
+        is_special = True
+    print(f"{current_number} -> {is_special}")
