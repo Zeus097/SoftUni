1488b29544d6c4a3619cec38943fca7039ac69a8
Vasil Dimitrov <142613528+Zeus097@users.noreply.github.com>
2024-01-22 00:39:46 +0200

    Add files via upload

---
+++ Python Fundamentals/Data_Types_and_Variables/gladiator_expenses.py
@@ -0,0 +1,17 @@
+lost_fights = int(input())
+helmet_price = float(input())
+sword_price = float(input())
+shield_price = float(input())
+armor_price = float(input())
+
+total_helmets_broken = lost_fights // 2
+total_swords_broken = lost_fights // 3
+total_shields_broken = lost_fights // (2 * 3)
+total_armor_broken = total_shields_broken // 2
+
+expenses = (total_helmets_broken * helmet_price +
+            total_swords_broken * sword_price +
+            total_shields_broken * shield_price +
+            total_armor_broken * armor_price)
+
+print(f"Gladiator expenses: {expenses:.2f} aureus")
