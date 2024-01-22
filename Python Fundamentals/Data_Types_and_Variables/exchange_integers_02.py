3dcb8c987a79735247e42139eb722038e501670b
Vasil Dimitrov <142613528+Zeus097@users.noreply.github.com>
2024-01-22 19:35:28 +0200

    Add files via upload

---
+++ Python Fundamentals/Data_Types_and_Variables/exchange_integers_02.py
@@ -0,0 +1,12 @@
+num_1 = int(input())
+num_2 = int(input())
+
+num_a = num_2
+num_b = num_1
+
+print("Before:")
+print(f"a = {num_1}")
+print(f"b = {num_2}")
+print("After:")
+print(f"a = {num_a}")
+print(f"b = {num_b}")
