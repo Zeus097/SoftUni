97ab17c073ab412b2bf5a965bf4741f7e7e3a983
Vasil Dimitrov <142613528+Zeus097@users.noreply.github.com>
2024-02-18 23:04:33 +0200

    Add files via upload

--- Python Fundamentals/exams/array_modifier.py
+++ Python Fundamentals/exams/array_modifier.py
@@ -1,33 +1,18 @@
-values = list(map(int, input().split()))
+array_of_integers = list(map(int, input().split()))
 command = input().split()
 
-
 while True:
-    command_as_string = command[0]
-    if command_as_string == "end":
+    if command[0] == "end":
         break
-
-    if command_as_string != "decrease":
-        first_command_number = int(command[1])
-        second_command_number = int(command[2])
-
-        if command_as_string == "swap":
-            values[first_command_number], values[second_command_number] =\
-                values[second_command_number], values[first_command_number]
-
-        elif command_as_string == "multiply":
-            values[first_command_number] *= values[second_command_number]
-
-    if command_as_string == "decrease":
-        modified_numbers = []
-        for number in values:
-            number -= 1
-            modified_numbers.append(number)
-        values.clear()
-        for number in modified_numbers:
-            values.append(number)
-
+    action = command[0]
+    if action == "swap":
+        index_1, index_2 = int(command[1]), int(command[2])
+        array_of_integers[index_1], array_of_integers[index_2] =\
+            array_of_integers[index_2], array_of_integers[index_1]
+    elif action == "multiply":
+        index_1, index_2 = int(command[1]), int(command[2])
+        array_of_integers[index_1] *= array_of_integers[index_2]
+    elif action == "decrease":
+        array_of_integers = [array - 1 for array in array_of_integers]
     command = input().split()
-
-modified_numbers = ", ".join(map(str, values))
-print(modified_numbers)
+print(*array_of_integers, sep=", ")
