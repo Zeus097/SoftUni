9dd71e8529d16f521ab39766a770addfa19c2b99
Vasil Dimitrov <142613528+Zeus097@users.noreply.github.com>
2024-03-18 12:43:38 +0200

    Add files via upload

---
+++ Python Fundamentals/Regular_Expressions/match_phone_number.py
@@ -0,0 +1,7 @@
+import re
+
+phone_numbers = input()
+pattern = r'\+359 2 [0-9]{3} [0-9]{4}|\+359-2-[0-9]{3}-[0-9]{4}\b'
+valid_numbers = re.findall(pattern, phone_numbers)
+
+print(", ".join(valid_numbers))
