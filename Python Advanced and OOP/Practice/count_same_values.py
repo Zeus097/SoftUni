f9379d3c7e03017ecb437e509e164513173ea5a7
Vasil Dimitrov <142613528+Zeus097@users.noreply.github.com>
2024-04-23 09:29:05 +0300

    Add files via upload

---
+++ Python Advanced and OOP/Practice/count_same_values.py
@@ -0,0 +1,7 @@
+nums = tuple(float(el) for el in input().split())
+occ = {}
+
+for num in nums:
+    if num not in occ:
+        occ[num] = nums.count(num)
+        print(f"{num} - {nums.count(num)} times")
