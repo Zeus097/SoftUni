2c430d8e4e5316bcbee6e9e30c4825210b7233ff
Vasil Dimitrov <142613528+Zeus097@users.noreply.github.com>
2024-06-18 22:29:07 +0300

    Add files via upload

---
+++ Python Advanced and OOP/Python Advanced/04_sum_of_coins.py
@@ -0,0 +1,27 @@
+def coin_sum(coins, target):
+    coins.sort(reverse=True)
+    used_coins = {}
+
+    index = 0
+    while target != 0 and index < len(coins):
+        count_coins = target // coins[index]
+        target %= coins[index]
+
+        if count_coins > 0:
+            used_coins[coins[index]] = count_coins
+
+        index += 1
+
+    if target != 0:
+        return "Error"
+
+    result = f"Number of coins to take: {sum(used_coins.values())}\n"
+    for value, count in used_coins.items():
+        result += f"{count} coin(s) with value {value}\n"
+
+    return result.strip()
+
+
+coins_list = list(map(int, input().split(', ')))
+target_sum = int(input())
+print(coin_sum(coins_list, target_sum))
