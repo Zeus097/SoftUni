8996af7314562e872ba8d8c39f034d01a4a653d5
Vasil Dimitrov <142613528+Zeus097@users.noreply.github.com>
2024-06-28 15:39:39 +0300

    Add files via upload

---
+++ Python Advanced and OOP/Python OOP/04_glass.py
@@ -0,0 +1,29 @@
+class Glass:
+    capacity = 250
+
+    def __init__(self):
+        self.content = 0
+
+    def fill(self, ml: int) -> str:
+        if self.content + ml <= self.capacity:
+            self.content += ml
+            return f"Glass filled with {ml} ml"
+        return f"Cannot add {ml} ml"
+
+    def empty(self) -> str:
+        self.content = 0
+        return "Glass is now empty"
+
+    def info(self) -> str:
+        space_left = abs(self.capacity - self.content)
+        return f"{space_left} ml left"
+
+
+# Test code
+
+# glass = Glass()
+# print(glass.fill(100))
+# print(glass.fill(200))
+# print(glass.empty())
+# print(glass.fill(200))
+# print(glass.info())
