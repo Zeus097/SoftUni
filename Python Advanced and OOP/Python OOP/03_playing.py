f6d4d2ad56510dbb8bdc5816ee1f0ec41c8ba6ab
Vasil Dimitrov <142613528+Zeus097@users.noreply.github.com>
2024-07-15 00:44:47 +0300

    Add files via upload

---
+++ Python Advanced and OOP/Python OOP/03_playing.py
@@ -0,0 +1,24 @@
+def start_playing(instance):
+    return instance.play()
+
+
+# Test_code
+
+# 01 / 02
+# class Guitar:
+#     def play(self):
+#         return "Playing the guitar"
+#
+#
+# guitar = Guitar()
+# print(start_playing(guitar))
+
+
+# 02 / 02
+# class Children:
+#     def play(self):
+#         return "Children are playing"
+#
+#
+# children = Children()
+# print(start_playing(children))
