793e249f97140e32dfa23a063db862eab30a409d
Vasil Dimitrov <142613528+Zeus097@users.noreply.github.com>
2024-07-15 00:44:35 +0300

    Add files via upload

---
+++ Python Advanced and OOP/Python OOP/02_image_area.py
@@ -0,0 +1,52 @@
+class ImageArea:
+    def __init__(self, width: int, height: int):
+        self.width = width
+        self.height = height
+
+    def get_area(self) -> int:
+        return self.width * self.height
+
+    def __gt__(self, other):
+        return self.get_area() > other.get_area()
+
+    def __ge__(self, other):
+        return self.get_area() >= other.get_area()
+
+    def __eq__(self, other):
+        return self.get_area() == other.get_area()
+
+    def __le__(self, other):
+        return self.get_area() <= other.get_area()
+
+    def __lt__(self, other):
+        return self.get_area() < other.get_area()
+
+
+# Test code
+
+
+# 01 / 03
+
+# a1 = ImageArea(7, 10)
+# a2 = ImageArea(35, 2)
+# a3 = ImageArea(8, 9)
+# print(a1 == a2)
+# print(a1 != a3)
+
+
+# 02 / 03
+
+# a1 = ImageArea(7, 10)
+# a2 = ImageArea(35, 2)
+# a3 = ImageArea(8, 9)
+# print(a1 != a2)
+# print(a1 >= a3)
+
+
+# 03 / 03
+
+# a1 = ImageArea(7, 10)
+# a2 = ImageArea(35, 2)
+# a3 = ImageArea(8, 9)
+# print(a1 <= a2)
+# print(a1 < a3)
