cb1723e12f5e5bf5157b5aedd5feb59ed5d538d3
Vasil Dimitrov <142613528+Zeus097@users.noreply.github.com>
2024-07-16 13:18:13 +0300

    Add files via upload

--- Python Advanced and OOP/Python OOP/01_vehicle.py
+++ Python Advanced and OOP/Python OOP/01_vehicle.py
@@ -1,15 +1,60 @@
-class Vehicle:
-    def __init__(self,  mileage: float, max_speed: int = 150):
-        self.mileage = mileage
-        self.max_speed = max_speed
-        self.gadgets = []
+from abc import ABC, abstractmethod
+
+
+class Vehicle(ABC):
+    CAR_AIR_CONDITIONER_CONSUMPTION = 0.9
+    TRUCK_AIR_CONDITIONER_CONSUMPTION = 1.6
+    TRUCK_FUEL_COEFFICIENT = 0.95
+
+    def __init__(self, fuel_quantity, fuel_consumption):
+        self.fuel_quantity = fuel_quantity
+        self.fuel_consumption = fuel_consumption
+
+    @abstractmethod
+    def drive(self, distance):
+        pass
+
+    @abstractmethod
+    def refuel(self, fuel):
+        pass
+
+
+class Car(Vehicle):
+    def drive(self, distance):
+        needed_fuel = (self.fuel_consumption + Vehicle.CAR_AIR_CONDITIONER_CONSUMPTION) * distance
+        self.fuel_quantity -= needed_fuel if needed_fuel <= self.fuel_quantity else 0
+        return self.fuel_quantity
+
+    def refuel(self, fuel):
+        self.fuel_quantity += fuel
+        return self.fuel_quantity
+
+
+class Truck(Vehicle):
+    pass
+
+    def drive(self, distance):
+        needed_fuel = (self.fuel_consumption + Vehicle.TRUCK_AIR_CONDITIONER_CONSUMPTION) * distance
+        self.fuel_quantity -= needed_fuel if needed_fuel <= self.fuel_quantity else 0
+        return self.fuel_quantity
+
+    def refuel(self, fuel):
+        self.fuel_quantity += fuel * Vehicle.TRUCK_FUEL_COEFFICIENT
 
 
 # Test code
+# Part I
 
-# car = Vehicle(20)
-# print(car.max_speed)
-# print(car.mileage)
-# print(car.gadgets)
-# car.gadgets.append('Hudly Wireless')
-# print(car.gadgets)
+# car = Car(20, 5)
+# car.drive(3)
+# print(car.fuel_quantity)
+# car.refuel(10)
+# print(car.fuel_quantity)
+
+# Part II
+
+# truck = Truck(100, 15)
+# truck.drive(5)
+# print(truck.fuel_quantity)
+# truck.refuel(50)
+# print(truck.fuel_quantity)
