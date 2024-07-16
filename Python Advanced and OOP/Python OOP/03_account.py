dcce4f31dba9d31215246d949e5405474651ddba
Vasil Dimitrov <142613528+Zeus097@users.noreply.github.com>
2024-07-16 13:18:45 +0300

    Add files via upload

--- Python Advanced and OOP/Python OOP/03_account.py
+++ Python Advanced and OOP/Python OOP/03_account.py
@@ -1,32 +1,82 @@
+from typing import List
+
+
 class Account:
-    def __init__(self, id_: int, name: str, balance: int = 0):
-        self.id = id_
-        self.name = name
-        self.balance = balance
+    def __init__(self, owner: str, amount: int = 0):
+        self.owner = owner
+        self.amount = amount
+        self._transactions: List[int] = []
 
-    def credit(self, amount: int) -> int:
-        self.balance += amount
-        return self.balance
+    @property
+    def balance(self):
+        return self.amount + sum(self._transactions)
 
-    def debit(self, amount: int):
-        if amount <= self.balance:
-            self.balance -= amount
-            return self.balance
-        return "Amount exceeded balance"
+    def handle_transaction(self, transaction_amount: int) -> str:
+        if self.balance + transaction_amount < 0:
+            raise ValueError("sorry cannot go in debt!")
+        self._transactions.append(transaction_amount)
+        return f"New balance: {self.balance}"
 
-    def info(self):
-        return f"User {self.name} with account {self.id} has {self.balance} balance"
+    def add_transaction(self, amount):
+        if not isinstance(amount, int):
+            raise ValueError("please use int for amount")
+        return self.handle_transaction(amount)
+
+    def __str__(self):
+        return f"Account of {self.owner} with starting amount: {self.amount}"
+
+    def __repr__(self):
+        return f"Account({self.owner}, {self.amount})"
+
+    def __len__(self):
+        return len(self._transactions)
+
+    def __getitem__(self, item):
+        return self._transactions[item]
+
+    def __reversed__(self):
+        return reversed(self._transactions)
+
+    def __gt__(self, other: 'Account'):
+        return self.balance > other.balance
+
+    def __ge__(self, other: 'Account'):
+        return self.balance >= other.balance
+
+    def __eq__(self, other: 'Account'):
+        return self.balance == other.balance
+
+    def __add__(self, other: 'Account'):
+        new_owner = f"{self.owner}&{other.owner}"
+        new_amount = self.amount + other.amount
+        new_account = Account(new_owner, new_amount)
+        new_account._transactions = self._transactions + other._transactions
+        return new_account
 
 
 # Test code
 
-# account = Account(1234, "George", 1000)
-# print(account.credit(500))
-# print(account.debit(1500))
-# print(account.info())
-#
-# account = Account(5411256, "Peter")
-# print(account.debit(500))
-# print(account.credit(1000))
-# print(account.debit(500))
-# print(account.info())
+# acc = Account('bob', 10)
+# acc2 = Account('john')
+# print(acc)
+# print(repr(acc))
+# acc.add_transaction(20)
+# acc.add_transaction(-20)
+# acc.add_transaction(30)
+# print(acc.balance)
+# print(len(acc))
+# for transaction in acc:
+#     print(transaction)
+# print(acc[1])
+# print(list(reversed(acc)))
+# acc2.add_transaction(10)
+# acc2.add_transaction(60)
+# print(acc > acc2)
+# print(acc >= acc2)
+# print(acc < acc2)
+# print(acc <= acc2)
+# print(acc == acc2)
+# print(acc != acc2)
+# acc3 = acc + acc2
+# print(acc3)
+# print(acc3._transactions)
