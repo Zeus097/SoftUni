419e35c3ac8b35b7a51a42eab865cc363dafc1e4
Vasil Dimitrov <142613528+Zeus097@users.noreply.github.com>
2024-07-23 12:33:03 +0300

    Add files via upload

---
+++ Python Advanced and OOP/Python OOP/06_fibonacci_generator.py
@@ -0,0 +1,13 @@
+def fibonacci():
+    current_num, next_num = 0, 1
+
+    while True:
+        yield current_num
+        current_num, next_num = next_num, current_num + next_num
+
+
+# # Test code
+#
+# generator = fibonacci()
+# for i in range(5):
+#     print(next(generator))
