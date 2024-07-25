80e8a0e60d3d2cef19e38d48b2654d612dc486e1
Vasil Dimitrov <142613528+Zeus097@users.noreply.github.com>
2024-07-26 00:47:56 +0300

    Add files via upload

---
+++ Python Advanced and OOP/Python OOP/04_type_check.py
@@ -0,0 +1,32 @@
+def type_check(variable_type):
+    def decorator(func):
+        def wrapper(*args):
+            for arg in args:
+                if not isinstance(arg, variable_type):
+                    return "Bad Type"
+            return func(*args)
+
+        return wrapper
+    return decorator
+
+
+# # Test code
+#
+# @type_check(int)
+# def times2(num):
+#     return num*2
+#
+#
+# print(times2(2))
+# print(times2('Not A Number'))
+
+
+# # Test code
+#
+# @type_check(str)
+# def first_letter(word):
+#     return word[0]
+#
+#
+# print(first_letter('Hello World'))
+# print(first_letter(['Not', 'A', 'String']))
