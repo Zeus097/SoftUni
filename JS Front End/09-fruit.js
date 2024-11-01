0db091e27ef68f66ee013406753dcd4b0eccfb45
Vasil Dimitrov <142613528+Zeus097@users.noreply.github.com>
2024-11-01 17:36:08 +0200

    Add files via upload

---
+++ JS Front End/09-fruit.js
@@ -0,0 +1,10 @@
+function solve(fruitType, weightInGrams, pricePerKg) {
+    let weightInKg = weightInGrams / 1000
+    let money = weightInKg * pricePerKg
+
+    console.log(`I need $${money.toFixed(2)} to buy ${weightInKg.toFixed(2)} kilograms ${fruitType}.`);
+    
+}
+
+solve('orange', 2500, 1.80) // I need $4.50 to buy 2.50 kilograms orange.
+solve('apple', 1563, 2.35) // I need $3.67 to buy 1.56 kilograms apple.
