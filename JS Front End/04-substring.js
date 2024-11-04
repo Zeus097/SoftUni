880066c7302f3c4cf3a981efb734e6b1e20aad58
Vasil Dimitrov <142613528+Zeus097@users.noreply.github.com>
2024-11-04 18:52:12 +0200

    Add files via upload

---
+++ JS Front End/04-substring.js
@@ -0,0 +1,10 @@
+function solve(string, startIndex, count) {
+    let sub = string.slice(startIndex, startIndex + count)
+
+    console.log(sub)
+
+}
+
+
+solve('ASentence', 1, 8) // Sentence
+solve('SkipWord', 4, 7) // Word
