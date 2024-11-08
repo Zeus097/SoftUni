1b3841c3a1f7d80133b2d5489db90f29db3c08af
Vasil Dimitrov <142613528+Zeus097@users.noreply.github.com>
2024-11-08 18:22:15 +0200

    Add files via upload

---
+++ JS Front End/01a-smallest-of-three-numbers.js
@@ -0,0 +1,6 @@
+solve = (a, b, c) => (Math.min(a, b, c))
+
+console.log(solve(2, 5, 3)) // 2
+console.log(solve(600, 342, 123)) // 123
+console.log(solve(25, 21, 4)) // 4
+console.log(solve(2, 2, 2)) // 2
