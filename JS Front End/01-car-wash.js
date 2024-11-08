80f0a848017e17ff8cefc63ea69e14397df7a7b7
Vasil Dimitrov <142613528+Zeus097@users.noreply.github.com>
2024-11-09 00:56:05 +0200

    Add files via upload

---
+++ JS Front End/01-car-wash.js
@@ -0,0 +1,34 @@
+function carWash(array) {
+    
+    let value = 0;
+
+    for (command of array) {
+        switch (command) {
+            case 'soap':
+                value += 10
+                break;
+
+            case 'water':
+                value *= 1.20
+                break;
+
+            case 'vacuum cleaner':
+                value *= 1.25
+                break;
+        
+            case 'mud':
+                value *= 0.90
+                break;
+        }
+    }
+
+    console.log(`The car is ${value.toFixed(2)}% clean.`);
+    
+}
+
+carWash(['soap', 'soap', 'vacuum cleaner', 'mud', 'soap', 'water'])
+// The car is 39.00% clean.
+
+
+carWash(["soap", "water", "mud", "mud", "water", "mud", "vacuum cleaner"]) 
+// The car is 13.12% clean.
