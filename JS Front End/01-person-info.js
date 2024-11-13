0b64ea84049092931dc9b4ef5acfb9ff16a60609
Vasil Dimitrov <142613528+Zeus097@users.noreply.github.com>
2024-11-13 23:57:29 +0200

    Add files via upload

---
+++ JS Front End/01-person-info.js
@@ -0,0 +1,10 @@
+function personInfo(firstName, lastName, age) {
+    
+    let person = {firstName: firstName, lastName: lastName, age: age};
+    return person;
+    
+}
+
+
+personInfo("Peter", "Pan", "20") // firstName: Peter, lastName: Pan, age: 20
+personInfo("George", "Smith", "18") // firstName: George, lastName: Smith, age: 18
