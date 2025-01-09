b618dd98acf4fc974daf13d88c65b4f220c86f9a
Vasil Dimitrov <142613528+Zeus097@users.noreply.github.com>
2025-01-09 20:46:43 +0200

    Add files via upload

---
+++ DB/PostgreSQL/lab_all_in_one.sql
@@ -0,0 +1,53 @@
+CREATE TABLE employees (
+	id SERIAL NOT NULL PRIMARY KEY,
+	first_name VARCHAR(30),
+	last_name VARCHAR(50),
+	hiring_date DATE DEFAULT '2023-01-01',
+	salary NUMERIC(10, 2),
+	devices_number INT
+);
+
+CREATE TABLE departments (
+	id SERIAL PRIMARY KEY NOT NULL,
+	name VARCHAR(50),
+	code CHAR(3),
+	description TEXT
+);
+
+CREATE TABLE issues (
+	id SERIAL PRIMARY KEY UNIQUE,
+	description VARCHAR(150),
+	date DATE,
+	start TIMESTAMP
+);
+
+
+ALTER TABLE employees
+ADD COLUMN middle_name VARCHAR(50)
+;
+
+
+ALTER TABLE employees
+ALTER COLUMN salary SET NOT NULL,
+ALTER COLUMN salary SET DEFAULT 0
+;
+
+ALTER TABLE employees
+ALTER COLUMN hiring_date SET NOT NULL
+;
+
+ALTER TABLE employees
+ALTER COLUMN middle_name TYPE VARCHAR(100)
+;
+
+
+TRUNCATE TABLE issues
+;
+
+DROP TABLE departments
+;
+
+
+
+
+
