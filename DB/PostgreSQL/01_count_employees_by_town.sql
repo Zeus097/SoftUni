d6732358fb1658bb9960cc5e3ea4dbeac2e000d0
Vasil Dimitrov <142613528+Zeus097@users.noreply.github.com>
2025-01-31 00:41:51 +0200

    Add files via upload

---
+++ DB/PostgreSQL/01_count_employees_by_town.sql
@@ -0,0 +1,31 @@
+CREATE OR REPLACE FUNCTION fn_count_employees_by_town(town_name VARCHAR(20))
+RETURNS INT
+AS
+$$
+	DECLARE
+		e_count INT;
+	BEGIN
+		SELECT
+			COUNT(e.employee_id) AS count INTO e_count
+		FROM
+			employees AS e
+		JOIN
+			addresses AS a
+		USING
+			(address_id)
+		JOIN
+			towns AS t
+		USING
+			(town_id)
+		WHERE
+			t.name = town_name;
+		RETURN e_count;
+	END;
+$$
+LANGUAGE plpgsql;
+
+SELECT fn_count_employees_by_town('Sofia') AS count;
+
+
+
+
