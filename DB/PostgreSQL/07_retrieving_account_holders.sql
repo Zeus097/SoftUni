056e513f8b069b23de69c2a898121316e8264750
Vasil Dimitrov <142613528+Zeus097@users.noreply.github.com>
2025-02-01 14:27:14 +0200

    Add files via upload

---
+++ DB/PostgreSQL/07_retrieving_account_holders.sql
@@ -0,0 +1,36 @@
+CREATE OR REPLACE PROCEDURE sp_retrieving_holders_with_balance_higher_than(
+    IN searched_balance NUMERIC
+)
+AS
+$$
+    DECLARE
+        holder_info RECORD; -- type is record from the table
+    BEGIN
+        FOR
+            holder_info in
+                SELECT
+                    ah.first_name || ' ' || ah.last_name AS full_name,
+                    SUM(a.balance) AS total_balance
+                FROM
+                    account_holders AS ah
+                JOIN
+                    accounts AS a
+                ON
+                    a.account_holder_id = ah.id
+                GROUP BY
+                    full_name
+                HAVING
+                    SUM(a.balance) > searched_balance
+                ORDER BY
+                    full_name
+        LOOP
+            RAISE NOTICE '% - %', holder_info.full_name, holder_info.total_balance;
+        END LOOP;
+    END;
+$$
+LANGUAGE plpgsql
+;
+
+-- PRIMARY OUTPUT
+-- 
+-- -- CALL sp_retrieving_holders_with_balance_higher_than(200000)
