f0c8df2874f6a84be6ddf6f0fc7a32af07104419
Vasil Dimitrov <142613528+Zeus097@users.noreply.github.com>
2025-02-03 17:50:36 +0200

    Add files via upload

---
+++ DB/PostgreSQL/10_animals_for_adoption.sql
@@ -0,0 +1,19 @@
+SELECT
+    a.name AS animal,
+    EXTRACT(YEAR FROM a.birthdate) AS birth_year,
+    at.animal_type
+FROM
+    animals AS a
+JOIN
+    animal_types AS at
+ON
+    at.id = a.animal_type_id
+WHERE
+    a.owner_id IS NULL
+    AND
+    AGE('01/01/2022', a.birthdate) < '5 years'
+    AND
+    at.animal_type <> 'Birds'
+ORDER BY
+    a.name
+;
