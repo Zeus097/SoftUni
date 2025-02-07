4d457c12c27236379314ca0ed0cbcba975cd0d92
Vasil Dimitrov <142613528+Zeus097@users.noreply.github.com>
2025-02-08 01:30:01 +0200

    Add files via upload

---
+++ DB/PostgreSQL/07_lucky_accounts.sql
@@ -0,0 +1,14 @@
+SELECT
+    CONCAT(a.id, ' ', a.username) AS id_username,
+    a.email
+FROM
+    accounts AS a
+JOIN
+    accounts_photos AS ap
+ON
+    a.id = ap.photo_id
+WHERE
+    a.id = ap.account_id
+ORDER BY
+    a.id
+;
