4b4638bc4da4a0d234d13300c762462de3d75151
Vasil Dimitrov <142613528+Zeus097@users.noreply.github.com>
2025-02-21 02:40:47 +0200

    Add files via upload

---
+++ DB/Python_ORM/03_1_choices.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+
+class OrderStatusChoices(models.TextChoices):
+    PENDING = 'Pending', 'Pending'
+    COMPLETED = 'Completed', 'Completed'
+    CANCELED = 'Canceled', 'Canceled'
