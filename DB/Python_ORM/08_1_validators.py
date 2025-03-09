5c544c5a04111fb3c10ffb68e9ea2bbb64d48d3d
Vasil Dimitrov <142613528+Zeus097@users.noreply.github.com>
2025-03-09 23:02:59 +0200

    Add files via upload

---
+++ DB/Python_ORM/08_1_validators.py
@@ -0,0 +1,10 @@
+from django.core.exceptions import ValidationError
+
+
+def name_validator(value):
+    for letter in value:
+        if not (letter.isalpha() or letter.isspace()):
+            raise ValidationError('Name can only contain letters and spaces')
+
+
+
