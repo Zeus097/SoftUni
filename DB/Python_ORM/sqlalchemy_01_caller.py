fed0da4f209f9db67a5afb56e113fac443caa305
Vasil Dimitrov <142613528+Zeus097@users.noreply.github.com>
2025-03-17 14:59:49 +0200

    Add files via upload

---
+++ DB/Python_ORM/sqlalchemy_01_caller.py
@@ -0,0 +1,104 @@
+"""
+# STEPS
+
+1. pip install sqlalchemy
+2. pip install alembic
+3. pip install psycopg2
+4. Create a database
+5. Create file for models
+6. Create the Base class
+7. alembic init alembic
+8. Tell alembic which is our base model target_metadata = Base.metadata
+9. Tell alembic how to connect to the db - sqlalchemy.url = postgresql+psycopg2://postgres:admin@localhost/sql_alchemy_exercise
+
+"""
+
+from typing import Type
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from helpers import handle_session
+from models import Recipe, Chef
+
+engine = create_engine("postgresql+psycopg2://postgres:0897535532@localhost/sqlalchemy_ex_db")
+Session = sessionmaker(bind=engine)
+session = Session()
+
+
+@handle_session(session)
+def create_recipe(name: str, ingredients: str, instructions: str) -> None:
+    new_recipe = Recipe(
+        name=name,
+        ingredients=ingredients,
+        instructions=instructions,
+    )
+
+    session.add(new_recipe)
+
+
+@handle_session(session)
+def update_recipe_by_name(name: str, new_name: str,  new_ingredients: str, new_instructions: str):
+    session.query(Recipe).filter_by(name=name).update({
+        Recipe.name: new_name,
+        Recipe.ingredients: new_ingredients,
+        Recipe.instructions: new_instructions,
+        })
+
+
+@handle_session(session)
+def delete_recipe_by_name(name: str):
+    session.query(Recipe).filter_by(name=name).delete()
+
+
+@handle_session(session)
+def get_recipes_by_ingredient(ingredient_name: str) -> list[Type[Recipe]]:
+    return session.query(Recipe).filter(
+        Recipe.ingredients.ilike(f"%{ingredient_name}%")
+    ).all()
+
+
+@handle_session(session)
+def swap_recipe_ingredients_by_name(first_recipe_name: str, second_recipe_name: str):
+    first_recipe = (
+        session.query(Recipe)
+        .filter_by(name=first_recipe_name)
+        .with_for_update()
+        .one()
+    )
+
+    second_recipe = (
+        session.query(Recipe)
+        .filter_by(name=second_recipe_name)
+        .with_for_update()
+        .one()
+    )
+
+    first_recipe.ingredients, second_recipe.ingredients = second_recipe.ingredients, first_recipe.ingredients
+
+
+@handle_session(session)
+def relate_recipe_with_chef_by_name(recipe_name: str, chef_name: str):
+    recipe = session.query(Recipe).filter_by(name=recipe_name).one()
+
+    if recipe.chef:
+        raise Exception(f"Recipe: {recipe_name} already has a related chef")
+
+    chef = session.query(Chef).filter_by(name=chef_name).one()
+    recipe.chef = chef
+
+    return f"Related recipe {recipe_name} with chef {chef_name}"
+
+
+def get_recipes_with_chef() -> str:
+    recipes_with_chef = (
+        session.query(Recipe.name, Chef.name)
+        .join(Chef, Recipe.chef)
+        .all()
+    )
+
+    return "\n".join(
+        f"Recipe: {recipe_name} made by chef: {chef_name}"
+        for recipe_name, chef_name in recipes_with_chef
+    )
+
