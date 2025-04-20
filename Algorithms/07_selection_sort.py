fe61e3da1462be8913245b0353f312183adc604e
Vasil Dimitrov <142613528+Zeus097@users.noreply.github.com>
2025-04-21 02:01:26 +0300

    Add files via upload

---
+++ Algorithms/07_selection_sort.py
@@ -0,0 +1,14 @@
+def selection_sort(nums):
+    for idx in range(len(nums)):
+        min_idx = idx
+
+        for cur_idx in range(idx + 1, len(nums)):
+            if nums[cur_idx] < nums[min_idx]:
+                min_idx = cur_idx
+
+        nums[idx], nums[min_idx] = nums[min_idx], nums[idx]
+
+
+nums_input = [int(x) for x in input().split()]
+selection_sort(nums_input)
+print(*nums_input)
