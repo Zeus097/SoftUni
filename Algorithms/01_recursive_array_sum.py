5ab27de6385b5aad1141c7ef55eae5682818e6c8
Vasil Dimitrov <142613528+Zeus097@users.noreply.github.com>
2025-04-21 02:00:22 +0300

    Add files via upload

--- Algorithms/01_recursive_array_sum.py
+++ Algorithms/01_recursive_array_sum.py
@@ -1,19 +1,9 @@
-# NOTE: In practice, this recursion should not be used here (instead use an iterative solution).
+def calc_recursive_array_sum(nums, idx=0):
+    if idx == len(nums) - 1:
+        return nums[idx]
+
+    return nums[idx] + calc_recursive_array_sum(nums, idx + 1)
 
 
-def arrau_sum(nums, index):
-    if index >= len(nums) - 1:
-        return nums[index]
-    
-    return nums[index] + arrau_sum(nums, index + 1)
-
-
-nums = list(map(int, input().split()))
-print(arrau_sum(nums, 0))
-
-
-# Input: 1 2 3 4
-# Output: 10
-
-# Input: -1 0 1
-# Output: 0
+array = [int(x) for x in input().split()]
+print(calc_recursive_array_sum(array))
